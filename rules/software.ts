a65acab545a611684a80f8f4b6ff6df5325f33a0
pamoller <kontakt@pamoller.com>
2024-07-03 20:10:59 +0200

    add description

--- rules/software.ts
+++ rules/software.ts
@@ -2,8 +2,12 @@ import { parseImports } from "npm:parse-imports@^2.1.0";
 import { DependencyError } from "../common/error.ts";
 import * as walk from "../common/walk.ts";
 import { globs } from "../common/glob.ts";
+/**
+* This module contains functions to check software dependencies in a software system.
+* @module
+*/
 
-// recurse directory glob and scan file imports for missing dependencies
+/**  recurse directory glob and scan file imports for missing dependencies **/
 export async function directoryDependsOn(dir: string, ...dependencies: string[]): Promise<boolean> {
   if (dependencies.length === 0)
     return true;
@@ -13,7 +17,7 @@ export async function directoryDependsOn(dir: string, ...dependencies: string[])
   return true;
 }
 
-// scan file imports for missing dependencies
+/** scan file imports for missing dependencies **/
 export async function fileDependsOn(path: string, ...dependencies: string[]): Promise<boolean> {
   if (dependencies.length === 0)
     return true;
@@ -33,7 +37,7 @@ export async function fileDependsOn(path: string, ...dependencies: string[]): Pr
   return true; 
 }
 
-// recurse directory glob and scan file imports for forbidden dependencies
+/**  recurse directory glob and scan file imports for forbidden dependencies **/
 export async function directoryDoesNotDependOn(dir: string, ...dependencies: string[]): Promise<boolean> {
   await walk.onGlob(dir, async (dir: string) => {
     await walk.onDir(dir, (path: string ) => fileDoesNotDependOn(path, ...dependencies));
@@ -41,7 +45,7 @@ export async function directoryDoesNotDependOn(dir: string, ...dependencies: str
   return true;
 }
 
-// scan file imports for forbidden dependencies
+/** scan file imports for forbidden dependencies **/
 export async function fileDoesNotDependOn(path: string, ...dependencies: string[]): Promise<boolean> {
   const code = Deno.readTextFileSync(path);
   specifier:
