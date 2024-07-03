c6f2648a11b43f72f5e004f75c4b4e1e52ee04d5
pamoller <kontakt@pamoller.com>
2024-07-03 20:46:05 +0200

    add description

--- README.md
+++ README.md
@@ -67,8 +67,8 @@ $ npx jsr add @pamoller/it-depends-on
 ````typescript
 import { describe, it } from "https://deno.land/std@0.196.0/testing/bdd.ts";
 import { assert } from "https://deno.land/std@0.196.0/assert/mod.ts";
-import * as layer from @jsr:@pamoller/it-depends-on;
-import * as software from @jsr:@pamoller/it-depends-on;
+import * as layer from 'jsr:@pamoller/it-depends-on@^0.1.4/layer';
+import * as software from 'jsr:@pamoller/it-depends-on@^0.1.4/software';
 
 describe("test layer policy", async () => {
     it("common is self containing", async () => {
@@ -79,5 +79,4 @@ describe("test layer policy", async () => {
         assert(layer.directoryDependsOn("./rules", "./common"));
         assert(layer.directoryDependsOn("./test", "./common"));
     });
-});
-````
+});````
--- mod.ts
+++ mod.ts
@@ -6,19 +6,19 @@
  * ```ts
  * import { describe, it } from "https://deno.land/std@0.196.0/testing/bdd.ts";
  * import { assert } from "https://deno.land/std@0.196.0/assert/mod.ts";
- * import * as layer from "./rules/layer.ts";
- *
+ * import * as layer from 'jsr:@pamoller/it-depends-on@^0.1.4/layer';
+ * import * as software from 'jsr:@pamoller/it-depends-on@^0.1.4/software';
+ * 
  * describe("test layer policy", async () => {
- *   it("common is strictly self containing", async () => {
- *      assert(layer.directoryDependsOn("./common"));
- *   });
- *
- *   it("rules and test are clients of common", async () => {
- *       assert(layer.directoryDependsOn("./rules", "./common"));
- *       assert(layer.directoryDependsOn("./test", "./common"));
- *   });
- *});
- * ```
+ *     it("common is self containing", async () => {
+ *         assert(layer.directoryDoesNotDependOn("./common"));
+ *     });
+ * 
+ *     it("rules and test are client of common", async () => {
+ *         assert(layer.directoryDependsOn("./rules", "./common"));
+ *         assert(layer.directoryDependsOn("./test", "./common"));
+ *     });
+ * });* ```
  *
  * @module
  */
